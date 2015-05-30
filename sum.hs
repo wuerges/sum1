c69f4f7e0093bcb1e38256aa1060383daefb7e85
Emilio Wuerges <wuerges@gmail.com>
2015-05-30 01:58:28 -0300

    fixed sum, added mult

--- sum.hs
+++ sum.hs
@@ -1,5 +1,6 @@
 import Numeric
 import System.Environment
+import Text.Printf
 
 readNum = map (fst . head . readHex . reverse . take 8) . takeWhile (not . null) . iterate (drop 8) . reverse
 
@@ -7,14 +8,27 @@ readNum = map (fst . head . readHex . reverse . take 8) . takeWhile (not . null)
 carry n = n `div` (0xffffffff+1)
 digit n = n `mod` (0xffffffff+1)
 
-sumDigits = zipWith (+) 
+
+sumDigits [] b = b
+sumDigits a [] = a
+sumDigits (a:as) (b:bs) = (a+b):(sumDigits as bs)
 
 sumCarrys c [] = [c]
 sumCarrys c (x:xs) = (digit $ x + c):(sumCarrys (carry $ x + c) xs)
 
 sumNums a b = sumCarrys 0 $ sumDigits a b 
 
-showNum = concat . map (\x -> showHex  x "") . reverse
+mul1Nums a b = sumCarrys 0 $ map (a*) b
+
+
+mulNums [] b = []
+mulNums (a:as) bs = mul1Nums a bs `sumNums` (0:mulNums as bs)
+
+
+printCasa :: Integer -> String
+printCasa = printf "%08x"
+
+showNum = concat . map printCasa . reverse
 
 
 main = do (a:b:[]) <- getArgs
